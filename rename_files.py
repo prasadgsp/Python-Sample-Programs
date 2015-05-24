340637a5f938467bcfe7820ac635e2b0fe922ada
Gowrisankar Prasad <prasad.gowrisankar@gmail.com>
2015-05-24 14:00:08 -0700

    Revert "Same programs"
    This reverts commit fe82d87b175e109f4467aa47a987727e8afc7ae4.

--- rename_files.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def rename_files():
- file_list=os.listdir(r"D:\online courses\U.D.A.C.I.T.Y\Programming Foundations with Python\prank")
- #print(file_list)
- os.chdir(r"D:\online courses\U.D.A.C.I.T.Y\Programming Foundations with Python\prank")
- for file_name in file_list:
-  os.rename(file_name,file_name.translate(None,"0123456789"))
-rename_files()
